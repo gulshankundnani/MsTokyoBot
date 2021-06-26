d9c61599b6efa6e49979297a6ab928bbd2938c4f
gulshankundnani <42181196+gulshankundnani@users.noreply.github.com>
2021-06-26 14:08:48 +0530

    Update MsTokyoBot.py

--- MsTokyoBot.py
+++ MsTokyoBot.py
@@ -90,7 +90,6 @@ print("Training Done")
 global con
 eventDict = {}
 eventDict[0] = [0]
-s = sched.scheduler(time.time, time.sleep)
 
 async def getDbCon():
     con = psycopg2.connect(database="mstokyodb", user="postgres", password="5miWLroSbKbkjFKO", host="mstokyodb-gb2gmf5iouhhn82v-svc.qovery.io", port="5432")
